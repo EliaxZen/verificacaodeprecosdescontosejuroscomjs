7cf99a0f4fedb0a784177dae3a94a15c513706a9
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-11-11 12:43:53 -0300

    adicionando funções

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 /*
-1- À vista Débito, recebe 10% de desconto;
-2- À vista no Dinheiro o PIX, recebe 15% de desconto;
-3- Em duas vezes, preço normal do produto sem juros;
-4- Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+  1- À vista Débito, recebe 10% de desconto;
+  2- À vista no Dinheiro o PIX, recebe 15% de desconto;
+  3- Em duas vezes, preço normal do produto sem juros;
+  4- Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 */
-//Dados
-let precoProduto = 100;
-let formaDePagamento = 4
-
-//Verificação
-if(formaDePagamento === 1) {
-  console.log("Valor total: R$" + (precoProduto - (precoProduto * 0.10)))
-} else if(formaDePagamento === 2) {
-  console.log("Valor total: R$" + (precoProduto - (precoProduto * 0.15)))
-} else if(formaDePagamento === 3) {
-  console.log("Valor total: R$" + precoProduto)
-} else {
-  console.log("Valor total: R$" + (precoProduto + (precoProduto * 0.10)))
+function aplicarDesconto(precoProduto, desconto) {
+  return precoProduto - (precoProduto * (desconto / 100))
 }
+
+function aplicarJuros(precoProduto, juros) {
+  return precoProduto + (precoProduto * (juros / 100))
+}
+
+function verificacaoPagamento(precoProduto, formaDePagamento) {
+  if (formaDePagamento === 1) {
+    return "Valor total: R$" + aplicarDesconto(precoProduto, 10);
+  } else if (formaDePagamento === 2) {
+    return "Valor total: R$" + aplicarDesconto(precoProduto, 30);
+  } else if (formaDePagamento === 3) {
+    return "Valor total: R$" + precoProduto;
+  } else {
+    return "Valor total: R$" + aplicarJuros(precoProduto, 10);
+  }
+}
+
+console.log(verificacaoPagamento(100, 4))
 
